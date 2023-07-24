89afe8ad08104bd5e82be8414155583685e44338
Bstar00 <samueloriola212@gmail.com>
2023-07-24 20:57:31 +0000

    Pascal function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -19,12 +19,22 @@ def pascal_triangle(n):
     """
     if type(n) is not int:
         raise TypeError("n must be an integer")
-    
     triangle = []
+    if n <= 0:
+        return triangle
+    previous = [1]
     for row_index in range(n):
-        rowlist = [1] * (row_index + 1)
-        for i in range(1, row_index):
-            rowlist[i] = triangle[row_index - 1][i - 1] + triangle[row_index - 1][i]
+        rowlist = []
+        if row_index == 0:
+            rowlist = [1]
+        else:
+            for i in range(row_index + 1):
+                if i == 0:
+                    rowlist.append(0 + previous[i])
+                elif i == (row_index):
+                    rowlist.append(previous[i - 1] + 0)
+                else:
+                    rowlist.append(previous[i - 1] + previous[i])
+        previous = rowlist
         triangle.append(rowlist)
-    
     return triangle
