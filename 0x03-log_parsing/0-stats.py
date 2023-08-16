7d2f22ffced875d29dcbf9b10f48b0865f12cd48
Bstar00 <samueloriola212@gmail.com>
2023-08-16 11:00:29 +0000

    log parse

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,54 @@
 #!/usr/bin/python3
-"""
-Log parsing script
-"""
-
 import sys
-import re
+import signal
 
-def output(log: dict) -> None:
-    """
-    Helper function to display statistics
-    """
-    print("Total file size: File size: {}".format(log["file_size"]))
-    for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
+# Initialize variables to hold metrics
+total_file_size = 0
+status_code_counts = {}
 
-if __name__ == "__main__":
-    regex = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')
+# Function to print metrics
+def print_metrics():
+    print(f"Total file size: {total_file_size}")
+    for status_code in sorted(status_code_counts):
+        print(f"{status_code}: {status_code_counts[status_code]}")
 
+# Handle keyboard interruption (CTRL + C)
+def signal_handler(sig, frame):
+    print_metrics()
+    sys.exit(0)
+
+# Set up signal handler for keyboard interruption
+signal.signal(signal.SIGINT, signal_handler)
+
+try:
     line_count = 0
-    log = {
-        "file_size": 0,
-        "code_frequency": {
-            str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]
-        },
-    }
-
-    try:
-        for line in sys.stdin:
-            line = line.strip()
-            match = regex.fullmatch(line)
-            if match:
-                line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
-
-                # File size
-                log["file_size"] += file_size
-
-                # Status code
-                if code.isdigit():
-                    log["code_frequency"][code] += 1
-
-                if line_count % 10 == 0:
-                    output(log)
-    finally:
-        output(log)
+    for line in sys.stdin:
+        line = line.strip()
+        parts = line.split()
+        
+        # Check if the line has enough parts to match the input format
+        if len(parts) >= 7:
+            ip_address = parts[0]
+            date = parts[2] + " " + parts[3]
+            status_code = parts[-3]
+            file_size = int(parts[-2])
+            
+            total_file_size += file_size
+            
+            # Check if the status code is a valid integer
+            if status_code.isdigit():
+                status_code = int(status_code)
+                if status_code in status_code_counts:
+                    status_code_counts[status_code] += 1
+                else:
+                    status_code_counts[status_code] = 1
+            
+            line_count += 1
+            
+            # Print metrics every 10 lines
+            if line_count % 10 == 0:
+                print_metrics()
+        
+except KeyboardInterrupt:
+    # Print metrics upon keyboard interruption
+    print_metrics()
